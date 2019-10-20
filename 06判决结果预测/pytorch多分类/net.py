a327f97b40c26f82cbbf505476875783f7c5b7d5
Zhang Shiwei <zhangshiwei17173@outlook.com>
2019-10-20 20:46:54 +0800

    实现案件判决结果的预测
    将预测问题当做一个多分类问题

---
+++ 06判决结果预测/pytorch多分类/net.py
@@ -0,0 +1,97 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+import csv
+
+
+class MyDataset(Dataset):
+    def __init__(self, csv_file):
+        self.df = np.loadtxt(csv_file, delimiter=',', dtype=np.float32)
+        self.x_data = torch.Tensor(self.df[:, :-1])
+        self.y_data = torch.LongTensor(self.df[:, -1])
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, idx):
+        return self.x_data[idx], self.y_data[idx]
+
+
+train_set = MyDataset("train5.csv")
+train_data = DataLoader(dataset=train_set, batch_size=32, shuffle=True)
+test_set = MyDataset("test5.csv")
+test_data = DataLoader(dataset=test_set, batch_size=32, shuffle=False)
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.layer1 = nn.Linear(5, 64)
+        self.layer2 = nn.Linear(64, 128)
+        self.layer3 = nn.Linear(128, 3)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = F.relu(x)
+        x = self.layer2(x)
+        x = F.relu(x)
+        return self.layer3(x)
+
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.00001)
+
+
+def train(epoch):
+    for batch_idx, (data, target) in enumerate(train_data):
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = net(data)
+
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 20 == 0:
+            print("Train Epoch: {} Loss: {:.6f}".format(epoch, loss.item()))
+
+
+def test():
+    test_loss = 0
+    # correct = 0
+    # total = 0
+    correct = list(0 for i in range(3))
+    total = list(0 for i in range(3))
+
+    for pattern, target in test_data:
+        pattern, target = Variable(pattern), Variable(target)
+        output = net(pattern)
+        test_loss += criterion(output, target).item()
+
+        prediction = torch.argmax(output, 1)
+
+        zipped = zip(pattern.data.tolist(), target.data.tolist(), prediction.tolist())
+        with open("result.csv", "a") as f:
+            f_csv = csv.writer(f)
+            f_csv.writerows(zipped)
+
+        res = (prediction == target).squeeze()
+        for i in range(len(target)):
+            label = target[i]
+            correct[label] += res[i].item()
+            total[label] += 1
+    for i in range(3):
+        print("accu of {} :{}".format(i, correct[i] / total[i]))
+        print("num of {} : {}".format(i, total[i]))
+    print(sum(correct) / sum(total))
+
+
+for epoch in range(1, 101):
+    train(epoch)
+test()
---
+++ 06判决结果预测/pytorch多分类/数据预处理.py
@@ -0,0 +1,109 @@
+import csv
+import numpy as np
+from random import shuffle
+
+
+def data_split(data_csv):
+    """
+    将原始数据分割成训练集和测试集
+    :param data_csv:原始数据
+    :return:
+    """
+    with open(data_csv) as f:
+        f_csv = csv.reader(f)
+        rows = list()
+        for row in f_csv:
+            rows.append(row)
+        del (rows[0])  # 去除表头
+        # 打乱顺序
+        # shuffle(rows)
+        rows_train = rows[:20000]
+        rows_test = rows[20000:]
+        with open("train16.csv", "w") as f_train:
+            f_train_csv = csv.writer(f_train)
+            f_train_csv.writerows(rows_train)
+        with open("test16.csv", "w") as f_test:
+            f_test_csv = csv.writer(f_test)
+            f_test_csv.writerows(rows_test)
+
+
+def patterns_weight(csv_file, new_csv_file):
+    """
+    给特征加权，然后将16个特征变为5个特征，以减轻数据的稀疏性
+    :param csv_file: 不带表头的源文件，16个特征
+    :param new_csv_file:加权之后的文件，5个特征
+    :return:
+    """
+    f1 = np.loadtxt(csv_file, delimiter=',', dtype=np.int)
+    new_csv_list = []
+    for i in range(len(f1)):
+        origin_patterns = f1[i]
+        temp_dict = dict()
+        temp_dict["01伤亡情况"] = origin_patterns[0] * 100 + origin_patterns[1] * 70  # 死亡人数+重伤人数
+        temp_dict["02责任认定"] = 10 if origin_patterns[2] == 1 else 8
+        # 酒后驾驶、吸毒后驾驶、无证驾驶、无牌驾驶、不安全驾驶、超载、逃逸
+        temp_dict["03违章情况"] = origin_patterns[3] * 120 + origin_patterns[4] * 130 + origin_patterns[5] * 100 + \
+                              origin_patterns[6] * 100 + origin_patterns[7] * 90 + origin_patterns[8] * 100 + \
+                              origin_patterns[9] * 130
+        # 抢救伤者、报警、现场等待、赔偿、认罪
+        temp_dict["04案后表现"] = origin_patterns[10] * (-90) + origin_patterns[11] * (-90) + origin_patterns[12] * (-90) + \
+                              origin_patterns[13] * (-120) + origin_patterns[14] * (-90)
+        temp_dict["05初犯偶犯"] = 120 if origin_patterns[15] == 0 else -90
+        temp_dict["06判决结果"] = origin_patterns[-1]
+        new_csv_list.append(temp_dict)
+    # 将新特征写回文件
+    headers = ["01伤亡情况", "02责任认定", "03违章情况", "04案后表现", "05初犯偶犯", "06判决结果"]
+    with open(new_csv_file, "w", newline="")as f2:
+        f_csv = csv.DictWriter(f2, headers)
+        # f_csv.writeheader()
+        f_csv.writerows(new_csv_list)
+
+
+def remove_error(csv_file1, csv_file2):
+    """
+    暴力消除数据的噪声
+    :param csv_file1:
+    :param csv_file2:
+    :return:
+    """
+    with open(csv_file1) as f1:
+        f1_csv = csv.reader(f1)
+        d = dict()
+        rows = list()
+        # 将原来的csv文件提炼成字典，key为特征，value为对应的判决结果组成的list
+        for row in f1_csv:
+            rows.append(row)
+            data = tuple(row[:-1])
+            label = row[-1]
+            if data in d:
+                d[data].append(label)
+            else:
+                d[data] = []
+                d[data].append(label)
+        # 生成正确的数据（每种特征组合对应的判决结果，取众数）
+        correct = []
+        # f3 = open(csv_file1)
+        # f3_csv = csv.reader(f3)
+        for row in rows:
+            new_row = row[:-1]
+            labels = d[tuple(new_row)]
+            true_label = max(labels, key=labels.count)
+            new_row.append(true_label)
+            correct.append(new_row)
+        # for key, value in d.items():
+        #     true_label = max(value, key=value.count)
+        #     new_row = list(key)
+        #     new_row.append(true_label)
+        #     correct.append(new_row)
+        with open(csv_file2, "w", newline="")as f2:
+            f2_csv = csv.writer(f2)
+            f2_csv.writerows(correct)
+
+
+data_split("data.csv")
+patterns_weight("train16.csv", "train5.csv")
+patterns_weight("test16.csv", "test5.csv")
+remove_error("train16.csv", "train16_without_error.csv")
+remove_error("test16.csv", "test16_without_error.csv")
+remove_error("train5.csv", "train5_without_error.csv")
+remove_error("test5.csv", "test5_without_error.csv")
