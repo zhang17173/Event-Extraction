b96b4cdd1e499775b480d8ca442432a8a237b196
Zhang Shiwei <zhangshiwei17173@outlook.com>
2019-10-20 21:14:56 +0800

    更新数据

--- 06判决结果预测/pytorch多分类/数据预处理.py
+++ 06判决结果预测/pytorch多分类/数据预处理.py
@@ -17,8 +17,8 @@ def data_split(data_csv):
         del (rows[0])  # 去除表头
         # 打乱顺序
         # shuffle(rows)
-        rows_train = rows[:20000]
-        rows_test = rows[20000:]
+        rows_train = rows[:12000]
+        rows_test = rows[12000:]
         with open("train16.csv", "w") as f_train:
             f_train_csv = csv.writer(f_train)
             f_train_csv.writerows(rows_train)
@@ -100,7 +100,7 @@ def remove_error(csv_file1, csv_file2):
             f2_csv.writerows(correct)
 
 
-data_split("data.csv")
+data_split("/home/zhangshiwei/Event-Extraction/06判决结果预测/特征提取/data.csv")
 patterns_weight("train16.csv", "train5.csv")
 patterns_weight("test16.csv", "test5.csv")
 remove_error("train16.csv", "train16_without_error.csv")
--- 06判决结果预测/pytorch多分类/结果分析.py
+++ 06判决结果预测/pytorch多分类/结果分析.py
@@ -3,10 +3,9 @@ from collections import Counter
 
 with open("result.csv", "r") as f:
     f_csv = csv.reader(f)
-    true_0, true_1, true_2, true_3 = 0, 0, 0, 0
-    false_0, false_1, false_2, false_3 = 0, 0, 0, 0
+    true_0, true_1, true_2 = 0, 0, 0
+    false_0, false_1, false_2 = 0, 0, 0
     pred_1 = []
-    pred_3 = []
     for row in f_csv:
         if row[-2] == '0':
             if row[-1] == row[-2]:
@@ -24,26 +23,20 @@ with open("result.csv", "r") as f:
                 true_2 += 1
             else:
                 false_2 += 1
-        # if row[-2] == '3':
-        #     if row[-1] == row[-2]:
-        #         true_3 += 1
-        #     else:
-        #         false_3 += 1
-        #         pred_3.append(row[-1])
-true_count = true_0 + true_1 + true_2 + true_3
-total_count = true_count + false_0 + false_1 + false_2 + false_3
+
+true_count = true_0 + true_1 + true_2
+total_count = true_count + false_0 + false_1 + false_2
 print("total:%d" % total_count)
 print("true:%d" % true_count)
 print("class" + '\t' + "total" + '\t' + "true" + '\t' + "false" + '\t')
 print("0" + '\t' + str(true_0 + false_0) + '\t' + str(true_0) + '\t' + str(false_0))
 print("1" + '\t' + str(true_1 + false_1) + '\t' + str(true_1) + '\t' + str(false_1))
 print("2" + '\t' + str(true_2 + false_2) + '\t' + str(true_2) + '\t' + str(false_2))
-print("3" + '\t' + str(true_3 + false_3) + '\t' + str(true_3) + '\t' + str(false_3))
-print(Counter(pred_1))
-print(Counter(pred_3))
+
 headers = ['01死亡人数', "02重伤人数", "04责任认定", "05是否酒后驾驶", "06是否吸毒后驾驶", "07是否无证驾驶", "08是否无牌驾驶", "09是否不安全驾驶", "10是否超载",
            "11是否逃逸", "12是否抢救伤者", "13是否报警", "14是否现场等待", "15是否赔偿", "16是否认罪", "18是否初犯偶犯", "判决结果"]
-f1 = open("preprocessed_data.txt", "r", encoding="utf-8")
+
+f1 = open("/home/zhangshiwei/Event-Extraction/01数据预处理/preprocessed_data.txt", "r", encoding="utf-8")
 cases = f1.readlines()
 f2 = open("result.csv", "r")
 f3 = open("test16_without_error.csv")
@@ -54,7 +47,7 @@ f4 = open("analysis.txt", "a", encoding="utf-8")
 f2_csv = list(csv.reader(f2))
 for i in range(len(f2_csv)):
     if f2_csv[i][1] == '1':
-        f4.write(cases[20000 + i])
+        f4.write(cases[12000 + i])
         f4.write(' '.join(f3_csv[i]) + '\n')
         for j in range(2, 16):
             if f3_csv[i][j] == '1':
