4db4525d7d478141ef02f7f258ee01894d93f9d5
Zhang Shiwei <zhangshiwei17173@outlook.com>
2019-10-21 14:22:00 +0800

    更新README

--- 01数据预处理/data_preprocess.py
+++ 01数据预处理/data_preprocess.py
@@ -14,14 +14,15 @@ for line in f1.readlines():
     location = line.split("\t")[0]
     line = line.split("\t")[-1]
     line1 = re.sub(u"（.*?）", "", line)  # 去除括号内注释
-    line2 = re.sub("[%s]+" % punctuation, "", line1)  # 去除标点
+    line2 = re.sub("[%s]+" % punctuation, "", line1)  # 去除标点、特殊字母
     # 去除冗余词
     line3 = re.sub(
         "本院认为|违反道路交通管理法规|驾驶机动车辆|因而|违反道路交通运输管理法规|违反交通运输管理法规|缓刑考.*?计算|刑期.*?止|依照|《.*?》|第.*?条|第.*?款|的|了|其|另|已|且",
         "",
         line2)
     # 删除内容过少或过长的文书，删除包含’保险‘的文书，只保留以’被告人‘开头的文书
-    if 100 < len(line3) < 400 and line3.startswith("被告人") and "保险" not in line3:
+    if 100 < len(line3) < 400 and line3.startswith(
+            "被告人") and "保险" not in line3:
         f2.write(str(num) + "\t" + location + '\t' + line3)
         num += 1
 
